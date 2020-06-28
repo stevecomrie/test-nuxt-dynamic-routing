28c09f98e46039467f4e526e559577a6c50095b2
Steve Comrie <steve@simplicate.ca>
2020-06-27 21:25:05 -0400

    Initial commit

---
+++ pages/passwords/_id.vue
@@ -0,0 +1,27 @@
+<template>
+   <main>
+      <p>
+         Password Used: {{password}}<br>
+         Data {{data}}
+      </p>
+   </main>
+</template>
+
+<script>
+   export default {
+      data: () => ({
+         data: '',
+         password : ''
+      }),
+
+      async asyncData ({ params }) {
+         //let data = await fetch( CALL_TO_YOUR_API + params.id + '.json' ).then(res => res.json())
+         let data = { fake : 'data', sample: 'returned from API' }
+
+         return {
+            data: data,
+            password: params.id
+         }
+      }
+   }
+</script>
